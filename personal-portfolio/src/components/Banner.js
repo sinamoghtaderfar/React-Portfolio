7987de23b327bdda12b82092ce760f64df9827ed
Sina Moghtaderfar <sinamoghtaderfard@gmail.com>
2023-11-13 21:33:08 +0330

    personal portfolio website - bannder

---
+++ personal-portfolio/src/components/Banner.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import {ArrowRightCircle} from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg"
+
+export const Banner = () => {
+
+    const [lopNum, setLopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+
+    const [text,setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+    const period = 2000;
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            tick();
+        },delta)
+        return ()=> {clearInterval(ticker)};
+    }, [text]);
+
+    const tick = ()=>{
+        let i =lopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length +1);
+
+        setText(updatedText);
+
+        if(isDeleting) setDelta(prevDelta => prevDelta / 2);
+
+        if(!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLopNum(lopNum + 1);
+            setDelta(500);
+        }
+
+    }
+
+    return (
+        <section className='banner' id='home'>
+            <Container>
+                <Row className='align-items-center'>
+                    <Col xs={12} md={6} xl={7}>
+                        <span className='tagline'>Welcome to my Portfolio</span>
+                        <h1>{`Hi I'm Sina Moghtaderfar `} <br/> <span className='wrap'>{text}</span> </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, sunt!</p>
+                        <button onClick={()=>console.log('conct')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={7}>
+                        <img src={headerImg} alt="Header Img"/>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
